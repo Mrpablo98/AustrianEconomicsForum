dd5861d890978a96ffe88795be39a3533eb5fe36
Pablo <pabloskyisan1998@gmail.com>
2023-04-22 01:16:44 +0200

    Inicio de proyecto de fin de ciclo

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/normalize.css">
+</head>
+<body>
+    <h1>AustrianEconomicsForum</h1>
+    
+<form action="login.php" method="POST" class="container1">
+    <div class="container">
+        
+        <div class="inputText">
+            <input placeholder="Nombre de usuario" type="text" name="username" id="username">
+            <input placeholder="Contraseña" type="password" name="password" id="password">
+        </div>
+    </div>
+    <button class="loginButton" type="submit">Log in</button>
+    <a href="register.php">Registrarse</a>
+</form>    
+<footer>
+    <p>© 2023 AustrianEconomicsForum</p>
+</footer>
+</body>
+
+</html>
