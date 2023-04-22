5c57fefc7d457d1e4c476d3922db132a50390482
Mrpablo98 <pabloskyisan1998@gmail.com>
2023-04-22 23:44:09 +0200

    Estilado dee la página log-in

---
+++ building.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+       body{
+        font-family: Helvetica, Arial, sans-serif;
+        font-size: 0.75vw;
+        background-color: #0A0C10;
+        color:white;
+        }
+        .title{
+            margin-top: 10%;
+            font-size: 8rem;
+            font-family: 'Courier New', Courier, monospace;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <p class=title>Sitio web en proceso de construcción. <br>Disculpa la molestias</p>
+    </div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,26 +5,67 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <h1>AustrianEconomicsForum</h1>
-    
-<form action="login.php" method="POST" class="container1">
-    <div class="container">
-        
-        <div class="inputText">
-            <input placeholder="Nombre de usuario" type="text" name="username" id="username">
-            <input placeholder="Contraseña" type="password" name="password" id="password">
+    <div>
+        <div class="title">
+            <p class="titleText">AustrianEconomicsForum</p>
         </div>
-    </div>
-    <button class="loginButton" type="submit">Log in</button>
-    <a href="register.php">Registrarse</a>
-</form>    
-<footer>
-    <p>© 2023 AustrianEconomicsForum</p>
+        <div class="Login-Logo">
+            <div class="logo-container">
+                <!--<p class="logo">AustrianEconomicsForum</p>-->
+                <img class="logo" src="img/Captura de pantalla 2023-04-22 214534.png">
+            </div>
+            <div class="form">  
+                <form action="building.php" method="POST" class="container1">
+                    <div class="container">
+                        
+                        <div class="inputText">
+                            <input placeholder="Nombre de usuario" type="text" name="username" id="username">
+                            <input placeholder="Contraseña" type="password" name="password" id="password">
+                        </div>
+                    </div>
+                    <button class="loginButton" type="submit">Log in</button>
+                    <a href="building.php">Registrarse</a>
+                </form>
+            </div>
+        </div>
+        <div class="disclaimer-container">
+            <p class="titleDisclaimer">Responsabilidad en el Tratamiento de Datos en AustrianEconomicsForum</p>  
+            <p class="disclaimer">
+                Bienvenido a "AsutrianEconomicsForum", un espacio donde la libertad, la responsabilidad individual y la protección de tus datos son nuestras prioridades.<br>
+                Nos inspiramos en los principios del libertarismo y la escuela austríaca de pensamiento económico para ofrecerte una plataforma única y segura.<br>
+                En esta Red Social Libertaria, entendemos que la propiedad privada y la libre asociación son pilares fundamentales de una sociedad justa y próspera.<br>
+                Tu información personal es, ante todo, tu propiedad, y nos comprometemos a respetarla y protegerla en todo momento siguiendo los principios del libertarismo.<br>
+                Con este objetivo como meta nos aseguramos de cumplir los siguientes principios:<br>  
+            </p> 
+    
+            <ul class="disclaimerList">
+                    <li>Consentimiento voluntario: Tú decides qué información compartes y con quién. No recopilamos,<br>
+                    almacenamos ni compartimos tus datos sin tu consentimiento explícito.</li>
+                    <li>Protección de datos y privacidad: Implementamos medidas de seguridad avanzadas y encriptación<br>
+                     de datos para garantizar que tu información esté protegida. Además, respetamos tu derecho a la privacidad,<br>
+                      asegurándonos de que puedas navegar y comunicarte en la plataforma sin temor a la vigilancia injustificada.</li>
+                    <li>Transparencia y responsabilidad: Nos comprometemos a ser transparentes en nuestras prácticas<br>
+                     y políticas de tratamiento de datos, manteniéndote siempre informado sobre cómo se utilizan y protegen tus datos.</li>
+                    <li>Autonomía y poder de decisión: Reconocemos que eres el único dueño y responsable de tus datos.<br>
+                     Por lo tanto, te proporcionamos las herramientas necesarias para administrar<br>
+                      y controlar tu información, incluida la posibilidad de eliminarla por completo de nuestra plataforma.</li>
+                    <li> Intercambio voluntario y cooperación: Fomentamos la cooperación voluntaria y el intercambio de ideas,<br>
+                     conocimientos y recursos entre los usuarios de nuestra plataforma,<br> siempre respetando la propiedad y privacidad de cada individuo.</li>
+            </ul>
+            <p class="disclaimer">Al unirte a AustrianEconomicsForum, asumes la responsabilidad de proteger tus propios datos y respetar los de los demás usuarios.<br>
+                    Juntos, podemos construir un espacio digital donde la libertad, la privacidad y la responsabilidad individual sean la base para establecer relaciones genuinas y enriquecedoras.<br>
+                    ¡Regístrate y comienza a disfrutar de la experiencia de una red social basada en los principios del libertarismo y la escuela austríaca!
+            </p>
+        </div>
+    </div> 
+    <footer>
+                <p>© 2023 AustrianEconomicsForum</p>
 </footer>
+
 </body>
 
 </html>
