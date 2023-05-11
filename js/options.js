1b426a55f4930a97a419797da38425f7253b189b
Mrpablo98 <pabloskyisan1998@gmail.com>
2023-05-11 16:28:13 +0200

    factorizar codigo options.js

--- js/options.js
+++ js/options.js
@@ -1,29 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-const optionsButton=document.getElementById('options-button');
-const optionsContainer=document.getElementById('options-container');
-const optionsForm=document.getElementById('options-form');
-const iconoOptions=document.getElementById('bars');
-var optionsOpen=false;
-optionsButton.addEventListener('click', function(){
-if(!optionsOpen){
-    optionsContainer.classList.remove('closed-options');
-    optionsContainer.classList.add('open-options');
-    optionsForm.classList.remove('closed-options');
-    optionsForm.classList.add('open-options');
-    iconoOptions.classList.remove('fa-bars');
-    iconoOptions.classList.add('fa-xmark');
-    optionsOpen=true;
-}else{
-    optionsContainer.classList.remove('open-options');
-    optionsContainer.classList.add('closed-options');
-    optionsForm.classList.remove('open-options');
-    optionsForm.classList.add('closed-options');
-    iconoOptions.classList.remove('fa-xmark');
-    iconoOptions.classList.add('fa-bars');
-    optionsOpen=false;
-}
-
-
-});
+    const optionsButton=document.getElementById('options-button');
+    const optionsContainer=document.getElementById('options-container');
+    const optionsForm=document.getElementById('options-form');
+    const iconoOptions=document.getElementById('bars');
+    var optionsOpen=false;
+    optionsButton.addEventListener('click', function(){
+        if(!optionsOpen){
+            optionsContainer.classList.remove('closed-options');
+            optionsContainer.classList.add('open-options');
+            optionsForm.classList.remove('closed-options');
+            optionsForm.classList.add('open-options');
+            iconoOptions.classList.remove('fa-bars');
+            iconoOptions.classList.add('fa-xmark');
+            optionsOpen=true;
+        }else{
+            optionsContainer.classList.remove('open-options');
+            optionsContainer.classList.add('closed-options');
+            optionsForm.classList.remove('open-options');
+            optionsForm.classList.add('closed-options');
+            iconoOptions.classList.remove('fa-xmark');
+            iconoOptions.classList.add('fa-bars');
+            optionsOpen=false;
+        }
+    });
 
 });
