3f4ddf50b6db060f12158aa29541be53034c15d5
Mrpablo98 <pabloskyisan1998@gmail.com>
2023-06-04 02:29:19 +0200

    tamaño archivo maximo aumentado

--- validate-publi.php
+++ validate-publi.php
@@ -5,8 +5,8 @@ require 'vendor/autoload.php';
 include('connection.php');
 use Google\Cloud\Storage\StorageClient;
 
-ini_set('upload_max_filesize', '100M');
-ini_set('post_max_size', '100M');
+ini_set('upload_max_filesize', '200M');
+ini_set('post_max_size', '200M');
 ini_set('max_execution_time', '300');
 ini_set('max_input_time', '300');
 
