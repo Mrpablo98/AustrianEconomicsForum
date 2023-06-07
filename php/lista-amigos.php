2c1054c4dc25bb365c049c1c13bed223c4c07e35
Mrpablo98 <pabloskyisan1998@gmail.com>
2023-06-07 21:39:02 +0200

    eliminar mensajes de error

--- php/lista-amigos.php
+++ php/lista-amigos.php
@@ -13,14 +13,11 @@ WHERE amigos.aceptada = 1
 AND (amigos.usuario_id1 = ? OR amigos.usuario_id2 = ?) 
 AND usuarios.id <> ?
 ";
-if(!$sql){
-    echo "Error: " . $sql . "<br>" . $mysqli->error;}
+
 $stmt = $mysqli->prepare($sql);
 $stmt->bind_param("iii", $userId, $userId, $userId);
 $stmt->execute();
-if(!$stmt){
-    echo "Error: " . $sql . "<br>" . $mysqli->error;
-}
+
 $result = $stmt->get_result();
 $amigos = [];
 while($row = $result->fetch_assoc()) {
