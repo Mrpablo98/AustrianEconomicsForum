f943bba7a0b3fa8918c1ef00676b6a5f017a76f4
Mrpablo98 <pabloskyisan1998@gmail.com>
2023-06-07 16:02:23 +0200

    orden posts en perfil

--- php/posts-perfil.php
+++ php/posts-perfil.php
@@ -6,7 +6,7 @@ $userId=$_SESSION['user']['user-id'];
 $start = isset($_GET['start']) ? intval($_GET['start']) : 0;
 $limit = isset($_GET['limit']) ? intval($_GET['limit']) : 15;
 $id = isset($_GET['id']) ? intval($_GET['id']) : $userId;
-$sql = $mysqli->prepare('SELECT posts.id, posts.usuario_id, posts.titulo, posts.contenido, posts.url_recurso, posts.tipo, usuarios.nombre FROM posts INNER JOIN usuarios ON posts.usuario_id = usuarios.id WHERE usuario_id = ? LIMIT ?, ?');
+$sql = $mysqli->prepare('SELECT posts.id, posts.usuario_id, posts.titulo, posts.contenido, posts.url_recurso, posts.tipo, usuarios.nombre FROM posts INNER JOIN usuarios ON posts.usuario_id = usuarios.id WHERE usuario_id = ? ORDER BY posts.id DESC LIMIT ?, ?');
 $sql->bind_param("iii", $id, $start, $limit);
 $sql->execute();
 $Postresult=$sql->get_result();
